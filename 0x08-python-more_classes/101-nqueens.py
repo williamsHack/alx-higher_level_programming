302a5950a71346f37ccc15216f67272e9165b708
williamsHack <williamyawdickson1998@gmail.com>
2023-04-04 00:25:43 +0000

    Chess grandmaster Judit Polgr, the strongest female chess player of all time
    The N queens puzzle is the challenge of placing N non-attacking queens on an NN chessboard. Write a program that solves the N queens problem.
    
    Usage: nqueens N
    If the user called the program with the wrong number of arguments, print Usage: nqueens N, followed by a new line, and exit with the status 1
    where N must be an integer greater or equal to 4
    If N is not an integer, print N must be a number, followed by a new line, and exit with the status 1
    If N is smaller than 4, print N must be at least 4, followed by a new line, and exit with the status 1
    The program should print every possible solution to the problem
    One solution per line
    Format: see example
    You committ have to print the solutions in a specific orderdon
    You are only allowed to import the sys module
    Read: Queen, Backtracking

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle"""
+# 101-nqueens.py
+"""Solves the N-queens puzzle.
 
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+
+Example:
+    $ ./101-nqueens.py N
+
+N must be an integer greater than or equal to 4.
+
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
 import sys
 
 
@@ -32,8 +49,10 @@ def get_solution(board):
 
 def xout(board, row, col):
     """X out spots on a chessboard.
+
     All spots where non-attacking queens can no
     longer be played are X-ed out.
+
     Args:
         board (list): The current working chessboard.
         row (int): The row where a queen was last played.
@@ -83,6 +102,7 @@ def xout(board, row, col):
 
 def recursive_solve(board, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
+
     Args:
         board (list): The current working chessboard.
         row (int): The current working row.
@@ -121,3 +141,7 @@ if __name__ == "__main__":
     solutions = recursive_solve(board, 0, 0, [])
     for sol in solutions:
         print(sol)
+    print("Found %d solutions" % len(solutions))
+
+    sys.exit(0)
+# End of 101-nqueens.py
