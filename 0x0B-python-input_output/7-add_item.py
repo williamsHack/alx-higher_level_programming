dd7b6ee4a4f6b2f6efe2844d971ec894b42170a7
williamsHack <williamyawdickson1998@gmail.com>
2023-04-16 17:08:17 +0000

    Write a script that adds all arguments to a Python list, and then save them to a file:
    You must use your function save_to_json_file from 5-save_to_json_file.py
    You must use your function load_from_json_file from 6-load_from_json_file.py
    The list must be saved as a JSON representation in a file named add_item.json
    If the file committ exist, it should be createddoesn
    You committ need to manage file permissions / exceptions.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
 
-""" Module that adds all arguments to a Python list, and then
-save them to a file
-"""
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
-from sys import argv
-import os.path
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-filename = "add_item.json"
-
-my_list = []
-
-if os.path.exists(filename):
-    if os.path.getsize(filename) == 0:
-        my_list = []
-    else:
-        my_list = load_from_json_file(filename)
-
-for args in argv[1:]:
-    my_list.append(args)
-
-save_to_json_file(my_list, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
